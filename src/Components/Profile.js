23df2e1ab82a30db32615482851ab4c2cc13a349
arun <karthikarun172@gmail.com>
2022-04-26 14:36:42 +0530

    navbar

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { useParams } from 'react-router'
+
 
 function Profile() {
-
-    let { id } = useParams()
-    console.log(id);
 
 
     return (
         <div>Profile
-            {id}
+
         </div>
     )
 }
